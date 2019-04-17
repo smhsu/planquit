dcbe48f5213373f2717c2c8c680ffe228004c94d
Silas Hsu <hsu.silas@wustl.edu>
2019-04-17 14:59:01 -0500

    Update siteBlacklist API

--- src/app/siteBlacklist.ts
+++ src/app/siteBlacklist.ts
@@ -1,5 +1,8 @@
 const BLACKLIST_STORAGE_KEY = "SITE_BLACKLIST";
 
+/**
+ * @return {string[]} the current site blacklist
+ */
 export function getBlacklist(): string[] {
     const data = window.localStorage.getItem(BLACKLIST_STORAGE_KEY);
     return data ? JSON.parse(data) : [];
@@ -9,17 +12,40 @@ function setBlacklist(blacklist: string[]) {
     window.localStorage.setItem(BLACKLIST_STORAGE_KEY, JSON.stringify(blacklist));
 }
 
-export function addSiteToBlacklist(domain: string) {
+/**
+ * Adds a domain to the blacklist.  Does nothing and returns `false` if the domain already exists.  Otherwise, returns
+ * `true`.
+ * 
+ * @param {string} domain - the domain to add to the site blacklist
+ * @return {boolean} whether the domain was added
+ */
+export function addSiteToBlacklist(domain: string): boolean {
     const list = getBlacklist();
-    list.push(domain);
-    setBlacklist(list);
+    const isNotOnList = list.indexOf(domain) === -1;
+    if (isNotOnList) {
+        list.push(domain);
+        setBlacklist(list);
+        return true;
+    } else {
+        return false;
+    }
 }
 
-export function removeSiteFromBlacklist(domain: string) {
+/**
+ * Removes a domain from the blacklist.  Does nothing and returns `false` if the domain doesn't exist.  Otherwise,
+ * returns `true`.
+ * 
+ * @param {string} domain - the domain to remove from the site blacklist
+ * @return {boolean} whether the domain was removed
+ */
+export function removeSiteFromBlacklist(domain: string): boolean {
     const list = getBlacklist();
-    const indexToRemove = list.findIndex(listItem => listItem === domain);
+    const indexToRemove = list.indexOf(domain);
     if (indexToRemove !== -1) {
         list.splice(indexToRemove, 1);
         setBlacklist(list);
+        return true;
+    } else {
+        return false;
     }
 }
