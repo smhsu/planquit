f167d8d0f0b14d2439810b829a016b5cf65fa3a2
Shiliang <zuo.shiliang@gmail.com>
2019-04-17 15:53:15 -0500

    siteselector

--- src/ui/siteSelector.ts
+++ src/ui/siteSelector.ts
@@ -11,9 +11,43 @@ export function renderSiteSelector(element: HTMLElement) {
     // append another <input type="text">
     // etc
     
-    addSiteToBlacklist("my site");
-    const blacklistSites = getBlacklist();
-    console.log(blacklistSites);
+    // addSiteToBlacklist("my site");
+    element.innerText = "Edit the sites you want to track\n\n";
+    var blacklistSites = getBlacklist();
+    let it : number = 0;
+    
+    for (let site of blacklistSites) {
+
+        var button = <HTMLButtonElement>document.createElement('button');
+        button.textContent = "Remove";
+        var siteString = <HTMLElement>document.createElement("site" + it);
+        siteString.innerText = " " + site + "\n\n";
+
+        button.onclick = function () {
+            removeSiteFromBlacklist(site);
+            //element.innerText = "";
+            renderSiteSelector(element);
+            return;
+        }
+
+        element.appendChild(button);
+        element.appendChild(siteString);
+
+        it = it + 1;
+    }
+
+    var inputString = <HTMLInputElement>document.createElement("input");
+    var button = <HTMLButtonElement>document.createElement("button");
+    button.textContent = "Add Site";
+    button.onclick = function() {
+        addSiteToBlacklist(inputString.value);
+        renderSiteSelector(element);
+    }
+
+    element.appendChild(button);
+    element.appendChild(inputString);
+
+
     // render BlackListSites
     
 
