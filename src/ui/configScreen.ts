4c796c93bfbb5086c5ff486a223b08f0b0185074
Shiliang <zuo.shiliang@gmail.com>
2019-04-23 18:15:14 -0500

    time field

--- src/ui/configScreen.ts
+++ src/ui/configScreen.ts
@@ -1,5 +1,4 @@
-
-
+import * as $ from "jquery";
 /**
  * Renders the config screen. 
  * 
@@ -9,7 +8,7 @@
  export function renderConfigScreen(element: HTMLElement) {
      element.innerHTML = '<br><strong>When do you want active discouragement?</strong><br>We will put up a warning screen when you visit a site that you want to visit less. <br><br>';
     
-     var form = document.createElement("FORM");
+     var form = document.createElement("div");
      element.appendChild(form);
 
      var alwaysRadio = <HTMLInputElement>document.createElement("INPUT");
@@ -24,26 +23,55 @@
      alwaysLabel.appendChild(document.createElement("br"));
      form.appendChild(alwaysLabel);
 
+     var neverRadio = <HTMLInputElement>document.createElement("input");
+     neverRadio.setAttribute("type", "radio");
+     neverRadio.setAttribute("name", "config");
+     neverRadio.setAttribute("value", "never");
+     neverRadio.setAttribute("id", "never");
+     var neverLabel = document.createElement("label");
+     neverLabel.setAttribute("for", "never");
+     neverLabel.appendChild(neverRadio);
+     neverLabel.appendChild(document.createTextNode("Never"));
+     neverLabel.appendChild(document.createElement("br"));
+     form.appendChild(neverLabel);
+
      var scheduledRadio = <HTMLInputElement>document.createElement("input");
      scheduledRadio.setAttribute("type", "radio");
      scheduledRadio.setAttribute("name", "config");
      scheduledRadio.setAttribute("value", "scheduled");
      scheduledRadio.setAttribute("id", "scheduled");
+
      var scheduledLabel = document.createElement("label");
      scheduledLabel.setAttribute("for", "scheduled");
      scheduledLabel.appendChild(scheduledRadio);
      scheduledLabel.appendChild(document.createTextNode("Scheduled"));
      scheduledLabel.appendChild(document.createElement("br"));
+
+
+     var scheduledTime = document.createElement("div");
+     var fromTime = <HTMLInputElement>document.createElement("input");
+     var toTime = <HTMLInputElement>document.createElement("input");
+     fromTime.setAttribute("type", "time");
+     toTime.setAttribute("type", "time");
+     scheduledTime.appendChild(fromTime);
+     scheduledTime.appendChild(toTime);
+     scheduledTime.hidden = !scheduledRadio.checked;
+     scheduledLabel.appendChild(scheduledTime);
      form.appendChild(scheduledLabel);
 
-     var manualRadio = <HTMLInputElement>document.createElement("input");
-     manualRadio.setAttribute("type", "radio");
-     manualRadio.setAttribute("name", "config");
-     manualRadio.setAttribute("value", "manual");
-     var manualLabel = document.createElement("label");
-     manualLabel.setAttribute("for", "manual");
-     manualLabel.appendChild(manualRadio);
-     manualLabel.appendChild(document.createTextNode("Manual Control"));
-     manualLabel.appendChild(document.createElement("br"));
-     form.appendChild(manualLabel);
+    $('input[type="radio"]').bind('click', function(){
+        // Processing only those that match the name attribute of the currently clicked button...
+        $('input[name="' + $(this).attr('name') + '"]').not($(this)).trigger('deselect'); // Every member of the current radio group except the clicked one...
+    });
+    
+    $('input[type="radio"]').bind('deselect', function(){
+        var id = ($(this).attr('id'));
+        if (id == "scheduled") {
+            scheduledTime.hidden = true;
+        }
+    });
+
+    $('#scheduled').click(function() {
+        scheduledTime.hidden = false;
+    });
  }
