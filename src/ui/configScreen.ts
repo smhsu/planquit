0ffa7fec3dda56deef01104372c0cc3c0b85d5da
Silas Hsu <hsu.silas@wustl.edu>
2019-04-23 22:35:15 -0500

    Finished config screen

--- src/ui/configScreen.ts
+++ src/ui/configScreen.ts
@@ -1,110 +1,117 @@
 import * as $ from "jquery";
 import * as moment from "moment";
+import { BlockMode, getBlockConfig, setBlockConfig } from "../app/blockTimes";
 
 const TIME_FORMAT = "hh:mm a";
+const DAYS = ["Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"];
+const DAYS_CSS_CLASS = "days-of-week";
 
-function parseTime(time: string) {
-    const parsed = moment.utc(this.startTime, TIME_FORMAT);
+function makeCheckboxes() {
+    const element = $("<div>");
+    for (let day of DAYS) {
+        const checkbox = $(`<div class="${DAYS_CSS_CLASS}"><input type="checkbox" /> ${day} </div>`);
+        element.append(checkbox);
+    }
+    return element[0];
+}
+
+(document.getElementById("scheduler") as HTMLElement).appendChild(makeCheckboxes());
+
+function showHideScheduler() {
+    if ($("#scheduled").prop("checked")) {
+        $("#scheduler").css("display", "block");
+    } else {
+        $("#scheduler").css("display", "none");
+    }
+}
+
+function fillInInputs() {
+    const config = getBlockConfig();
+    let radioToCheck = $();
+    switch (config.mode) {
+        case BlockMode.alwaysOn:
+            radioToCheck = $("#always");
+            break;
+        case BlockMode.alwaysOff:
+            radioToCheck = $("#never");
+            break;
+        case BlockMode.scheduled:
+            radioToCheck = $("#scheduled");
+            break;
+        default:
+            ;
+    }
+    radioToCheck.prop("checked", true);
+    showHideScheduler();
+
+    const start = moment().hours(config.schedule.startHour).minutes(config.schedule.startMinute);
+    const end = moment().hours(config.schedule.endHour).minutes(config.schedule.endMinute);
+    $("#from-time").val(start.format("HH:mm"));
+    $("#to-time").val(end.format("HH:mm"));
+
+    const daysCheckboxes = $(`.${DAYS_CSS_CLASS} input[type="checkbox"]`);
+    for (let checkedIndex of config.schedule.daysOfWeek) {
+        (daysCheckboxes[checkedIndex] as HTMLInputElement).checked = true;
+    }
+}
+
+fillInInputs();
+$('input[type="radio"]').click(showHideScheduler);
+
+function parseSchedule() {
+    const daysCheckboxes = $(`.${DAYS_CSS_CLASS} input[type="checkbox"]`);
+    const daysOfWeek = [];
+    for (let i = 0; i < daysCheckboxes.length; i++) {
+        if ((<HTMLInputElement> daysCheckboxes[i]).checked) {
+            daysOfWeek.push(i);
+        }
+    }
+
+    if (daysOfWeek.length === 0) {
+        throw new Error("Check at least one day");
+    }
+
+    const startTime = moment.utc($("#from-time").val(), TIME_FORMAT);
+    if (!startTime.isValid()) {
+        throw new Error("Enter a valid start time");
+    }
+    const endTime = moment.utc($("#to-time").val(), TIME_FORMAT);
+    if (!endTime.isValid()) {
+        throw new Error("Enter a valid end time");
+    }
+
+    if (endTime.isSameOrBefore(startTime)) {
+        throw new Error("Start time must be before end time");
+    }
+
     return {
-        hours: parsed.hours(),
-        minutes: parsed.minutes()
+        daysOfWeek,
+        startHour: startTime.hours(),
+        startMinute: startTime.minutes(),
+        endHour: endTime.hours(),
+        endMinute: endTime.minutes()
     };
 }
 
-/**
- * Renders the config screen. 
- * 
- * @param {HTMLElement} element - the element to append the plan editor UI to
- */
-
-export function renderConfigScreen(element: HTMLElement) {
-    /*
-    element.innerHTML = '<br><strong>When do you want active discouragement?</strong><br>We will put up a warning screen when you visit a site that you want to visit less. <br><br>';
-
-    var form = document.createElement("div");
-    element.appendChild(form);
-
-    var alwaysRadio = <HTMLInputElement>document.createElement("INPUT");
-    alwaysRadio.setAttribute("type", "radio");
-    alwaysRadio.setAttribute("name", "config");
-    alwaysRadio.setAttribute("value", "always");
-    alwaysRadio.setAttribute("id", "always");
-    var alwaysLabel = document.createElement("label");
-    alwaysLabel.setAttribute("for", "always");
-    alwaysLabel.appendChild(alwaysRadio);
-    alwaysLabel.appendChild(document.createTextNode("Always"));
-    alwaysLabel.appendChild(document.createElement("br"));
-    form.appendChild(alwaysLabel);
-
-    var neverRadio = <HTMLInputElement>document.createElement("input");
-    neverRadio.setAttribute("type", "radio");
-    neverRadio.setAttribute("name", "config");
-    neverRadio.setAttribute("value", "never");
-    neverRadio.setAttribute("id", "never");
-    var neverLabel = document.createElement("label");
-    neverLabel.setAttribute("for", "never");
-    neverLabel.appendChild(neverRadio);
-    neverLabel.appendChild(document.createTextNode("Never"));
-    neverLabel.appendChild(document.createElement("br"));
-    form.appendChild(neverLabel);
-
-    var scheduledRadio = <HTMLInputElement>document.createElement("input");
-    scheduledRadio.setAttribute("type", "radio");
-    scheduledRadio.setAttribute("name", "config");
-    scheduledRadio.setAttribute("value", "scheduled");
-    scheduledRadio.setAttribute("id", "scheduled");
-
-    var scheduledLabel = document.createElement("label");
-    scheduledLabel.setAttribute("for", "scheduled");
-    scheduledLabel.appendChild(scheduledRadio);
-    scheduledLabel.appendChild(document.createTextNode("Scheduled"));
-    scheduledLabel.appendChild(document.createElement("br"));
-
-
-    var scheduledTime = document.createElement("div");
-    var fromTime = <HTMLInputElement>document.createElement("input");
-    var toTime = <HTMLInputElement>document.createElement("input");
-    fromTime.setAttribute("type", "time");
-    toTime.setAttribute("type", "time");
-    scheduledTime.appendChild(fromTime);
-    scheduledTime.appendChild(toTime);
-    scheduledTime.hidden = !scheduledRadio.checked;
-    scheduledLabel.appendChild(scheduledTime);
-    form.appendChild(scheduledLabel);
-    */
-}
-
-function setTimeHidden(status: boolean) {
-    var fromTime = document.getElementById("fromTime");
-    var toTime = document.getElementById("toTime");
-    var days = document.getElementById("days");
-    if (fromTime) {
-        fromTime.hidden = status;
+$("#save").click(() => {
+    const config = getBlockConfig();
+    if ($("#always").prop("checked")) {
+        config.mode = BlockMode.alwaysOn;
+    } else if ($("#never").prop("checked")) {
+        config.mode = BlockMode.alwaysOff;
+    } else {
+        config.mode = BlockMode.scheduled;
     }
-    if (toTime) {
-        toTime.hidden = status;
+
+    if (config.mode === BlockMode.scheduled) {
+        try {
+            config.schedule = parseSchedule();
+        } catch (e) {
+            $("#messages").text(e.message).css("color", "red");
+            return;
+        }
     }
-    if (days) {
-        days.hidden = status;
-    }
-}
 
-$(document).ready(function() {
-    setTimeHidden(true);
-})
-
-$('input[type="radio"]').bind('click', function(){
-    // Processing only those that match the name attribute of the currently clicked button...
-    $('input[name="' + $(this).attr('name') + '"]').not($(this)).trigger('deselect'); // Every member of the current radio group except the clicked one...
-});
-
-$('input[type="radio"]').bind('deselect', function(){
-    var id = ($(this).attr('id'));
-    if (id == "scheduled") {
-        setTimeHidden(true);
-    }
-});
-
-$("#scheduled").click(function() {
-    setTimeHidden(false);
+    setBlockConfig(config);
+    $("#messages").text("Saved!").css("color", "green");
 });
