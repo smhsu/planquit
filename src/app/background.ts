eeee2a0e94fad0151c1dd945a6cecd6aa2d4556a
Silas Hsu <hsu.silas@wustl.edu>
2019-05-01 00:28:59 -0500

    Update docs

--- src/app/background.ts
+++ src/app/background.ts
@@ -1,6 +1,5 @@
 import { Tracker } from "./Tracker";
 import { SiteLog } from "./SiteLog";
-import { getPlan } from "./QuitPlan";
 import { Blocker } from "./Blocker";
 
 const blocker = new Blocker();
